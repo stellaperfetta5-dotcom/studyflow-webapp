f8941ee6d42fc45a4bdde8afb0558e426033c21e
stellaperfetta5-dotcom <stellaperfetta5@gmail.com>
2025-12-27 10:15:01 +0100

    Update script (1).js

--- script (1).js
+++ script (1).js
@@ -1,15 +1,1 @@
-function addTask() {
-    const input = document.getElementById("taskInput");
-    const text = input.value.trim();
-
-    if (text === "") return;
-
-    const li = document.createElement("li");
-    li.innerHTML = `
-        ${text}
-        <button onclick="this.parentElement.remove()">✖</button>
-    `;
-
-    document.getElementById("taskList").appendChild(li);
-    input.value = "";
-}
+Delete script (1).html
